8dcbb390554f28fbe5e2f5038d916222727322ca
raffenio <mintaka.star@gmail.com>
2026-06-08 23:52:27 -0700

    Change cron config to take Mexico city 3 am

--- src/main/java/com/raffenio/christmascountdown/scheduler/TweetScheduler.java
+++ src/main/java/com/raffenio/christmascountdown/scheduler/TweetScheduler.java
@@ -22,8 +22,8 @@ public class TweetScheduler {
         this.twitterService = twitterService;
     }
 
-    // Ejecuta todos los días a las 9:00 AM (zona horaria configurada en application.properties)
-    @Scheduled(cron = "${tweet.schedule.cron:0 0 9 * * *}", zone = "${tweet.schedule.timezone:America/Argentina/Buenos_Aires}")
+    // Ejecuta todos los días a las 3:00 AM (zona horaria configurada en application.properties)
+    @Scheduled(cron = "${tweet.schedule.cron:0 0 3 * * *}", zone = "${tweet.schedule.timezone:America/Mexico_City}")
     public void sendDailyChristmasCountdown() {
         log.info("Iniciando tarea programada - {}", LocalDateTime.now());
 
